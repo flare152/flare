509742f26ba37b52e14650a979389dea0f0c7ea8
10101 <hugowork112@gmail.com>
2025-02-25 11:29:12 +0800

    打包完成

--- protobuf-codegen/build.rs
+++ protobuf-codegen/build.rs
@@ -1,22 +1,29 @@
 use std::path::PathBuf;
 
 fn main() {
-    let proto_files = vec!["proto/net.proto"];
-    let proto_dir = PathBuf::from("proto");
-    let out_dir = PathBuf::from("./src/flare_gen");
-
-    // 创建输出目录
-    std::fs::create_dir_all(&out_dir).unwrap();
+    // 定义.proto文件及其对应的输出目录
+    let proto_files = vec![
+        ("proto/net.proto", "src/flare_gen"),
+       // ("proto/user.proto", "src/flare_gen/user"),
+    ];
 
     // 配置 prost-build
     let mut config = prost_build::Config::new();
-    config.out_dir(&out_dir);
-    
-    // 编译 proto 文件
-    config.compile_protos(&proto_files, &[proto_dir]).unwrap();
 
-    // 让 cargo 在 proto 文件改变时重新运行
-    for proto in proto_files {
+    for (proto, out_dir) in proto_files {
+        let proto_dir = PathBuf::from("proto");
+        let out_path = PathBuf::from(out_dir);
+
+        // 创建输出目录
+        std::fs::create_dir_all(&out_path).unwrap();
+
+        // 设置输出目录
+        config.out_dir(&out_path);
+
+        // 编译 proto 文件
+        config.compile_protos(&[proto], &[proto_dir]).unwrap();
+
+        // 让 cargo 在 proto 文件改变时重新运行
         println!("cargo:rerun-if-changed={}", proto);
     }
-} 
+}
