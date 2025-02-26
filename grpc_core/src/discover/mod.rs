08d7f262c5247db4e14fef52a4b767815b3aa290
10101 <hugowork112@gmail.com>
2025-02-26 10:51:13 +0800

    etcd作为注册中心测试通过

--- grpc_core/src/discover/mod.rs
+++ grpc_core/src/discover/mod.rs
@@ -3,8 +3,8 @@ mod registry;
 mod etcd;
 
 pub use registry::{Registry, Registration};
-pub use consul::ConsulDiscover;
-pub use etcd::EtcdDiscover;
+pub use consul::{ConsulDiscover, ConsulConfig,ConsulRegistry};
+pub use etcd::{EtcdDiscover, EtcdConfig, EtcdRegistry};
 
 // Re-export volo's Discover trait and types
 pub use volo::discovery::{Discover, Instance, Change};
